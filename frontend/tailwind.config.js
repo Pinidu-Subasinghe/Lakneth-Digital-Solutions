623d5bbd11a1597902880782d7de62d34e9c2533
pinidu2002 <pinidusubasinghe@gmail.com>
2025-11-06 10:57:23 +0530

    font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito Sans", "sans-serif"],
+      },
+    },
+  },
   darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
   plugins: [],
 };
